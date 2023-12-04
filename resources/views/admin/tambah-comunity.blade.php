412d74cdee77c774791a21f55705b3a55685fadb
Viqri12 <viqrifadhillah77@gmail.com>
2023-12-04 12:40:59 +0700

    update

--- resources/views/admin/tambah-comunity.blade.php
+++ resources/views/admin/tambah-comunity.blade.php
@@ -20,7 +20,7 @@
             pointer-events: auto; 
         }
 </style>
-        <div class="mt-3 mb-2 d-flex align-items-center col-8 ms-3">
+        <div class="mt-3 mb-2 d-md-flex align-items-center col-md-8 ms-3">
             <span class="fw-bold" style="font-size: 24px;">Input  KOL Comunity</span>
         </div>
             <div class="row border mb-4 col-11 ms-3 bg-white shadow rounded-3">
@@ -32,7 +32,7 @@
             <form action="{{ route('tambah-comunity')}}" method="POST">
                     @csrf
                 <div class="row g-3 mb-3 p-3 col-12">
-                    <div class=" col-4 ">
+                    <div class=" col-md-4 ">
                         <div class="">
                             <label class="fw-bold form-label">Nama Comunity</label>
                         </div>
@@ -40,7 +40,7 @@
                             <input type="text" name="name_comunity" class=" form-control col-12"  placeholder="Masukan Nama">
                         </div>
                     </div>
-                    <div class=" col-4 ">
+                    <div class=" col-md-4 ">
                         <div class="">
                             <label class="fw-bold form-label ">Username</label>
                         </div>
@@ -48,7 +48,7 @@
                             <input type="text" name="username" class=" form-control col-12" placeholder="Masukan Username">
                         </div>
                     </div>
-                    <div class=" col-4 ">
+                    <div class=" col-md-4 ">
                         <div class="">
                             <label class="fw-bold form-label ">Followers</label>
                         </div>
@@ -56,7 +56,7 @@
                             <input type="text" name="followers" class=" form-control col-12" placeholder="Masukan Jumlah Followers">
                         </div>
                     </div> 
-                    <div class=" col-4 ">
+                    <div class=" col-md-4 ">
                         <div class="">
                             <label class="fw-bold form-label ">Instagram Link</label>
                         </div>
@@ -64,7 +64,7 @@
                             <input type="text" name="comunity_link" class=" form-control col-12" placeholder="wwww.tiktok.com/p/CvO8pb8rkm_/">
                         </div>  
                     </div> 
-                    <div class=" col-4  cursor-pointer">
+                    <div class=" col-md-4  cursor-pointer">
                         <div class="">
                             <label class="fw-bold form-label ">Tier</label>
                         </div>
@@ -76,7 +76,7 @@
                             <option value="Mega">Mega</option>
                         </select>
                     </div>
-                    <div class=" col-4 ">
+                    <div class=" col-md-4 ">
                         <div class="">
                             <label class="fw-bold form-label ">Ig Photo Feed</label>
                         </div>
@@ -84,7 +84,7 @@
                             <input type="text" name="ig_photo_feed" class=" form-control col-12" placeholder="">
                         </div>  
                     </div> 
-                    <div class=" col-4 ">
+                    <div class=" col-md-4 ">
                         <div class="">
                             <label class="fw-bold form-label ">Ig Photo Feed Carousel</label>
                         </div>
@@ -92,7 +92,7 @@
                             <input type="text" name="ig_carousel" class=" form-control col-12" placeholder="">
                         </div>  
                     </div> 
-                    <div class=" col-4 ">
+                    <div class=" col-md-4 ">
                         <div class="">
                             <label class="fw-bold form-label ">Ig Story</label>
                         </div>
@@ -100,7 +100,7 @@
                             <input type="text" name="ig_story" class=" form-control col-12" placeholder="">
                         </div>  
                     </div> 
-                    <div class=" col-4 ">
+                    <div class=" col-md-4 ">
                         <div class="">  
                             <label class="fw-bold form-label ">Ig Reels</label>
                         </div>
@@ -108,7 +108,7 @@
                             <input type="text" name="ig_reels" class=" form-control col-12" placeholder="">
                         </div>  
                     </div> 
-                    <div class=" col-4 ">
+                    <div class=" col-md-4 ">
                         <div class="">
                             <label class="fw-bold form-label ">Ig Live</label>
                         </div>
@@ -116,7 +116,7 @@
                             <input type="text" name="id_live" class=" form-control col-12" placeholder="">
                         </div>  
                     </div> 
-                    <div class=" col-4 ">
+                    <div class=" col-md-4 ">
                         <div class="">
                             <label class="fw-bold form-label ">Attending Event</label>
                         </div>
@@ -124,7 +124,7 @@
                             <input type="text" name="attending_event" class=" form-control col-12" placeholder="">
                         </div>  
                     </div> 
-                    <div class=" col-4  cursor-pointer">
+                    <div class=" col-md-4">
                         <div class="">
                             <label class="fw-bold form-label ">Pilih Category</label>
                         </div>
@@ -136,7 +136,7 @@
                             <option value="komunitas Olahraga">Komunitas Olahraga</option>
                         </select>
                     </div>
-                    <div class=" col-4 ">
+                    <div class=" col-md-4 ">
                         <div class="">
                             <label class="fw-bold form-label ">Domisili</label>
                         </div>
@@ -144,7 +144,7 @@
                             <input type="text" name="domisili" class=" form-control col-12" placeholder="Masukan Domisili">
                         </div>  
                     </div>      
-                    <div class=" col-4 ">
+                    <div class=" col-md-4 ">
                         <div class="">
                             <label class="fw-bold form-label">Notes</label>
                         </div>
@@ -152,7 +152,7 @@
                             <textarea id="deskripsi" name="notes" class="form-control" name="deskripsi" cols="48"></textarea>
                         </div> 
                     </div>
-                    <div class="d-flex justify-content-end col-4" style="">
+                    <div class="d-flex justify-content-end col-md-4" style="">
                         <button  class="bg-transparent border-0">
                             <img src="{{ asset('assets-baru/images/img.svg')}}" alt="">
                         </button>
